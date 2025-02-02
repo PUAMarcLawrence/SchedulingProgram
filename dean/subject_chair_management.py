0275213834929f1caab994ae9a5875256e0b685a
PUAMarcLawrence <83485910+PUAMarcLawrence@users.noreply.github.com>
2025-02-02 22:35:45 +0800

    subject chair page

--- dean/subject_chair_management.py
+++ dean/subject_chair_management.py
@@ -9,31 +9,42 @@ def add_DepartmentProgram():
         else:
             st.error("Program already exists.")
 
-# def remove_DepartmentProgram():
+def remove_program():
+    if st.session_state['selected_program']:
+        if remove_program(st.session_state['selected_program']):
+            st.success("Program removed successfully.")
+        else:
+            st.error("Program does not exist.")
+
+def remove_DepartmentProgram():
+    select_box,button = st.columns(2)
+    selected_program = select_box.selectbox(
+        "Select a Program to remove:",
+        get_subjectChair_Dean("Subject Chair",st.session_state['department_ID'])['Program'].unique(),
+        key="selected_program")
+    button.button(
+        "Remove", 
+        on_click=remove_program)
 
 def add_new():
-    # Input box for adding new items
     text_box,button = st.columns(2)
     text_box.text_input(
         "Add new Program:", 
         help="Enter a Program Code Example(ECE,CPE,EE etc)",
         key="new_program",
-        label_visibility="visible")
+        label_visibility="visible",)
     button.button(
         "Add", 
         on_click=add_DepartmentProgram)
-    button.button(
-        "Remove", 
-        on_click=st.rerun)
-
 
 # Main app layout
 st.title("Subject Chair List")
-
-add_new()
-
+option = st.selectbox("Choose an option",["Add New Program","Remove Program"])
+if option == "Add New Program":
+    add_new()
+else:
+    remove_DepartmentProgram()
 st.dataframe(
     get_subjectChair_Dean("Subject Chair",st.session_state['department_ID']),
         use_container_width=True,
         hide_index=True)
-        # num_rows="dynamic")
