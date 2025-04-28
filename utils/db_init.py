4690e8b665ec9d95144a4aa1658271cbe62a8841
Marc Lawrence C. Pua <marc_pua@yahoo.com>
2025-04-28 16:53:31 +0800

    update

--- utils/db_init.py
+++ utils/db_init.py
@@ -25,5 +25,51 @@ def initialize_db():
         with sqlite3.connect(users_db) as conn:
             conn.execute('PRAGMA foreign_keys = ON;')
             conn.execute(
-                
+                '''CREATE TABLE IF NOT EXISTS users (
+                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT NOT NULL UNIQUE,
+                    password TEXT NOT NULL,
+                    role TEXT NOT NULL
+                )'''
             )
+            conn.execute(
+                '''CREATE TABLE IF NOT EXISTS departments (
+                    department_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL UNIQUE
+                    daen_id INTEGER NOT NULL,
+                    FOREIGN KEY (dean_id) REFERENCES users (user_id) ON DELETE CASCADE
+                )'''
+            )
+            conn.execute(
+                '''CREATE TABLE IF NOT EXISTS programs (
+                    program_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL UNIQUE,
+                    chair_id INTEGER NOT NULL,
+                    department_id INTEGER NOT NULL,
+                    FOREIGN KEY (chair_id) REFERENCES users (user_id) ON DELETE CASCADE,
+                    FOREIGN KEY (department_id) REFERENCES departments (department_id) ON DELETE CASCADE
+                )'''
+            )
+            conn.execute(
+                '''CREATE TABLE IF NOT EXISTS courses (
+                    course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    code TEXT NOT NULL UNIQUE,
+                    name TEXT NOT NULL,
+                    lec_hrs INTEGER,
+                    lab_hrs INTEGER,
+                    credits INTEGER NOT NULL,
+                    department_id INTEGER NOT NULL,
+                    FOREIGN KEY (department_id) REFERENCES departments (department_id) ON DELETE CASCADE
+                )'''
+            )
+            conn.execute(
+                '''CREATE TABLE IF NOT EXISTS schedules (
+                    schedule_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    course_id INTEGER NOT NULL,
+                    semester TEXT NOT NULL,
+                    year INTEGER NOT NULL,
+                    FOREIGN KEY (course_id) REFERENCES courses (course_id) ON DELETE CASCADE
+                )'''
+            )
+    except sqlite3.OperationalError as e:
+        print(f"Error initializing database: {e}")
