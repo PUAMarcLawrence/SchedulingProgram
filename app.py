31ad6aa934bccd6ce377f6cd13faeecd0f2d1343
PUAMarcLawrence <83485910+PUAMarcLawrence@users.noreply.github.com>
2025-04-30 22:57:50 +0800

    Refactor database initialization to consolidate database connections and improve error handling

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from utils.db_init import initialize_db
 
 if "role" not in st.session_state:
     st.session_state.role = None
+if "loggedIn" not in st.session_state:
+    st.session_state.loggedIn = False
+if "pageLogin" not in st.session_state:
+    st.session_state.pageLogin = True
 
 ROLES = [None, "Admin", "Dean", "Subject CHair"]
 
@@ -36,11 +40,11 @@ if st.session_state['loggedIn'] and len(page_dict) > 0 :
     pg = st.navigation( {"Account": account_pages} | page_dict)
 else:
     initialize_db()
-    if check_anyUser():
-        pg = st.navigation([st.Page(AdminRegistration)])
-    else:
-        if st.session_state['pageLogin']==True:
-            pg = st.navigation([st.Page(login)])
-        else:
-            pg = st.navigation([st.Page(register)])
-pg.run()
+    # if check_anyUser():
+    #     pg = st.navigation([st.Page(AdminRegistration)])
+    # else:
+    #     if st.session_state['pageLogin']==True:
+    #         pg = st.navigation([st.Page(login)])
+    #     else:
+    #         pg = st.navigation([st.Page(register)])
+# pg.run()
--- utils/db_init.py
+++ utils/db_init.py
@@ -2,30 +2,15 @@ import sqlite3
 import os
 
 db_path = './data'
-users_db = db_path +'/users.db'
-departments_db = db_path +'/departments.db'
-programs_db = db_path +'/programs.db'
-courses_db = db_path +'/courses.db'
-schedules_db = db_path +'/schedules.db'
+university_db = db_path +'/university.db'
 
 def initialize_db():
     if not os.path.exists(db_path):
         os.makedirs(db_path)
-    if not os.path.exists(users_db):
-        os.makedirs(users_db)
-    if not os.path.exists(departments_db):
-        os.makedirs(departments_db)
-    if not os.path.exists(programs_db):
-        os.makedirs(programs_db)
-    if not os.path.exists(courses_db):
-        os.makedirs(courses_db)
-    if not os.path.exists(schedules_db):
-        os.makedirs(schedules_db)
-    with sqlite3.connect(users_db) as conn:
-        cursor = conn.cursor()
+    with sqlite3.connect(university_db) as conn:
         try:
-            cursor.execute('PRAGMA foreign_keys = ON;')
-            cursor.execute(
+            conn.execute('PRAGMA foreign_keys = ON;')
+            conn.execute(
                 '''CREATE TABLE IF NOT EXISTS users (
                     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     username TEXT NOT NULL UNIQUE,
@@ -33,46 +18,14 @@ def initialize_db():
                     role TEXT NOT NULL
                 )'''
             )
-            cursor.execute(
+            conn.execute(
                 '''CREATE TABLE IF NOT EXISTS departments (
                     department_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL UNIQUE
-                    daen_id INTEGER NOT NULL,
-                    FOREIGN KEY (dean_id) REFERENCES users (user_id) ON DELETE CASCADE
-                )'''
-            )
-            cursor.execute(
-                '''CREATE TABLE IF NOT EXISTS programs (
-                    program_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL UNIQUE,
-                    chair_id INTEGER NOT NULL,
-                    department_id INTEGER NOT NULL,
-                    FOREIGN KEY (chair_id) REFERENCES users (user_id) ON DELETE CASCADE,
-                    FOREIGN KEY (department_id) REFERENCES departments (department_id) ON DELETE CASCADE
+                    daen_id INTEGER UNIQUE,
+                    FOREIGN KEY (daen_id) REFERENCES users(user_id) ON DELETE SET NULL
                 )'''
             )
-            cursor.execute(
-                '''CREATE TABLE IF NOT EXISTS courses (
-                    course_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    code TEXT NOT NULL UNIQUE,
-                    name TEXT NOT NULL,
-                    lec_hrs INTEGER,
-                    lab_hrs INTEGER,
-                    credits INTEGER NOT NULL,
-                    department_id INTEGER NOT NULL,
-                    FOREIGN KEY (department_id) REFERENCES departments (department_id) ON DELETE CASCADE
-                )'''
-            )
-            cursor.execute(
-                '''CREATE TABLE IF NOT EXISTS schedules (
-                    schedule_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    course_id INTEGER NOT NULL,
-                    semester TEXT NOT NULL,
-                    year INTEGER NOT NULL,
-                    FOREIGN KEY (course_id) REFERENCES courses (course_id) ON DELETE CASCADE
-                )'''
-            )
-            conn.commit()
+            
         except sqlite3.OperationalError as e:
-            conn.rollback()
             print(f"Error creating tables: {e}")
